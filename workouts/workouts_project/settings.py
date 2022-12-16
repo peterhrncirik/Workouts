657e5926ba879f10c71fd2f7f1b1b4fff7cfe880
Peter Hrnčírik <42555620+peterhrncirik@users.noreply.github.com>
2022-12-16 12:56:14 +0100

    few updates

--- workouts/workouts_project/settings.py
+++ workouts/workouts_project/settings.py
@@ -10,7 +10,7 @@ env.read_env()
 SECRET_KEY = env.str("SECRET_KEY")
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-env.bool("DEBUG", default=False)
+DEBUG = env.bool("DEBUG", default=False)
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
